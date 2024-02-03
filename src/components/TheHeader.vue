f05c09bb4c0787e4225906b9f2c70b157b906f6c
alirezaJavaheri <alireza.jvh98@gmail.com>
2024-02-04 00:21:43 +0100

    feat: create header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,11 @@
+<script lang="ts"></script>
+
+<template>
+    <div
+        class="h-[56px] self-stretch py-0 px-md flex justify-center items-center border-b border-solid border-border-divider"
+    >
+        <img src="@/assets/logo.png" class="h-10" />
+    </div>
+</template>
+
+
