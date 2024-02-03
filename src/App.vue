cda1a1ace00494e441f19905f78dc661c55ba630
alirezaJavaheri <alireza.jvh98@gmail.com>
2024-02-04 00:31:25 +0100

    feat: create footer component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import TheHeader from '@/components/TheHeader.vue'
+import TheFooter from '@/components/TheFooter.vue'
 </script>
 
 <template>
   <TheHeader />
+  <div class="h-full min-h-[calc(100vh-106px)]">
+    hello
+  </div>
+  <TheFooter />
 </template>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,11 @@
+<script lang="ts"></script>
+
+<template>
+    <div class="w-full h-12 bg-white border-t border-gray-300 lg:fixed bottom-0">
+      <div class="flex justify-center items-center h-full font-normal">
+        ©{YEAR} Book Club.
+      </div>
+    </div>
+</template>
+
+
