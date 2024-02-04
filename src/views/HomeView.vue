dcb5c5281f49736011f9f4a07b292a1d94fb72df
alirezaJavaheri <alireza.jvh98@gmail.com>
2024-02-04 14:34:16 +0100

    feat: simple ui of homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import InputField from '@/components/InputField.vue'
-
+import BaseButton from '@/components/BaseButton.vue'
 const email = ref('')
+
+function test() {
+    console.log('click on test');
+}
 </script>
 
 <template>
     <div class="pt-20 pb-10 flex flex-col justify-center items-center gap-6">
-        <InputField v-model="email" label="salam" placeholder="test"  />
+        <InputField v-model="email" label="Email" placeholder="Enter your Email" />
+        <BaseButton  text="login" @click="test()" />
     </div>
 </template>
