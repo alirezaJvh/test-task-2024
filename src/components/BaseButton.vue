3721dff529422eab786941fe57c930a07c4bea70
alirezaJavaheri <alireza.jvh98@gmail.com>
2024-02-04 14:33:19 +0100

    feat: button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+
+type ButtonProps = {
+    text: string;
+    disabled?: boolean
+}
+
+defineProps<ButtonProps>();
+
+const emit = defineEmits(['click']);
+
+function onClickHandler() {
+    emit('click')
+}
+</script>
+
+<template>
+    <button 
+        @click="onClickHandler" 
+        :disabled="disabled" 
+        class="h-[30px] px-4 bg-orange-500 hover:bg-orange-400 disabled:bg-orange-300 text-sm rounded-sm text-white" 
+        type="button">
+        {{ text }}
+    </button>
+</template>
