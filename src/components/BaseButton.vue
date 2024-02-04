7218da362a301a37667d541f5e5efc6b84426510
alirezaJavaheri <alireza.jvh98@gmail.com>
2024-02-04 19:49:07 +0100

    feat: add card component

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -18,7 +18,7 @@ function onClickHandler() {
     <button 
         @click="onClickHandler" 
         :disabled="disabled" 
-        class="h-[30px] px-4 bg-orange-500 hover:bg-orange-400 disabled:bg-orange-300 text-sm rounded-sm text-white" 
+        class="h-[30px] px-4 bg-orange-500 hover:bg-orange-400 disabled:bg-orange-300 text-sm rounded text-white" 
         type="button">
         {{ text }}
     </button>
---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import BaseButton from '@/components/BaseButton.vue'
+
+type CardProps = {
+    image?: string;
+    title?: string;
+}
+
+defineProps<CardProps>()
+
+</script>
+
+<template>
+
+
+<div class="bg-white border border-gray-200 rounded-lg">
+    <div v-if="image">
+        <img class="rounded-t-lg" :src="image"  />
+    </div>
+    <div class="p-5">
+        <div v-if="title" class="flex justify-between items-center border-b border-gray-200 pb-1 mb-3">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ title }}</h5>
+            <BaseButton text="Add Book" />
+        </div>
+        <div>
+            <slot></slot>
+        </div>
+    </div>
+</div>
+
+
+</template>
