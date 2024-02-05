dcbacfa5efbf6e4c3a4703a1d605342cf300a3ec
alirezaJavaheri <alireza.jvh98@gmail.com>
2024-02-05 10:04:48 +0100

    docs(card): add story of card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,7 +2,6 @@
 import BaseButton from '@/components/BaseButton.vue'
 
 type CardProps = {
-    image?: string;
     title?: string;
     action?: string;
 }
@@ -21,9 +20,6 @@ function onClickButton() {
 
 
 <div class="bg-white border border-gray-200 rounded-lg">
-    <div v-if="image">
-        <img class="rounded-t-lg" :src="image"  />
-    </div>
     <div class="p-5">
         <div v-if="title" class="flex justify-between items-center border-b border-gray-200 pb-2 mb-3">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ title }}</h5>
